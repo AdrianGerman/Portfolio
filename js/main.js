ed905d22826b3afb3d5a6dfcb4ccbb9398e96e89
AdrianGerman <adrian.german1019@gmail.com>
2024-03-15 22:46:47 -0600

    finish main styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function cambiarIdioma(idioma) {
-  const nuevoArchivo = idioma === "es" ? "/index.html" : "/index-en.html";
+  const nuevoArchivo = idioma === "es" ? "index.html" : "index-en.html";
   window.location.href = nuevoArchivo;
 }
